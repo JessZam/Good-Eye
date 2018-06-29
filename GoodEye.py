045dec8bfdcba878b24fa6afe94576baf449163c
Ryan <40242649+rodrigue13@users.noreply.github.com>
2018-06-29 19:53:13 -0400

    Add files via upload

---
+++ GoodEye.py
@@ -0,0 +1,10 @@
+'''GoodEye - Dickson/Jess/Ryan'''
+import pygame
+
+pygame.init()
+
+GREY = (115,115,115)
+size = (1280,720)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("GoodEye");
