b5b290274701f02cd8902cc5edfc76c41e373cb0
JessZam <38445173+JessZam@users.noreply.github.com>
2018-07-29 18:14:45 -0400

    Update GoodEye.py

--- GoodEye.py
+++ GoodEye.py
@@ -3,40 +3,30 @@
 import pygame, os, sys
 from pygame.locals import *
 from sys import exit
-from PyQt5 import QtWidgets
 
 pygame.init()
-gameExit = False #for switches
-gameOver = False #for switches
 
-
-#------------------------------------------ Declarations -----------------------------------------------------------#
-screen_width = 1000
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+# Game Resolution
+screen_width=1000
+screen_height=600
+screen=pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("GoodEye")
 clock = pygame.time.Clock()
-FPS = 30
-
-#--------------------------------------- Declarations ENDS ---------------------------------------------------------#
+FPS=30
 
 
-
-#------------------------------------------- Text Renderer ------------------------------------------------------------#
+# Text Renderer
 def text_format(message, textFont, textSize, textColor):
-    newFont = pygame.font.Font(textFont, textSize)
-    newText = newFont.render(message, 0, textColor)
+    newFont=pygame.font.Font(textFont, textSize)
+    newText=newFont.render(message, 0, textColor)
 
     return newText
 
-#------------------------------------------- Text Renderer ENDS -------------------------------------------------------#
 
-
-
-#----------------------------------------------- Colors ---------------------------------------------------------------#
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN_YELLOW = (173, 255, 47)
+# Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN_YELLOW = (173,255,47)
 BLUE = (0, 0, 255)
 GRAY = (197, 197, 197)
 YELLOW = (255, 255, 0)
@@ -49,131 +39,287 @@ SKY_BLUE = (0, 128, 255)
 NAVY_BLUE = (0, 0, 128)
 font = "freesansbold.ttf"
 
-#----------------------------------------------- Colors ENDS ----------------------------------------------------------#
-
-
-
-
-#--------------------------------------------------- MAIN MENU --------------------------------------------------------#
+#------------------------------------------------------------ MAIN MENU --------------------------------------------#
 
 def main_menu():
-    menu = True
-    selected = "start"
+    # Every global variable needs to be referenced
+    global clock
+    menu=True
+    selected="start"
+
+    start_blit = None       # Declare variables before assignment
+    quit_blit = None
 
     while menu:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type==pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    selected = "start"
-                elif event.key == pygame.K_DOWN:
-                    selected = "quit"
-                if event.key == pygame.K_RETURN:
-                    if selected == "start":
+            if event.type==pygame.MOUSEBUTTONUP:
+                if event.button==1:
+                    # Use collision enabled objects to figure which of the 2 buttons were clicked
+                    if start_blit.collidepoint(event.pos):
+                        print("Collision Start")
+                        selected = "start"
                         menu = False
-                        game_loop()
-                    if selected == "quit":
+
+                    elif quit_blit.collidepoint(event.pos):
+                        print("Collision quit")
+                        selected = "quit"
                         pygame.quit()
                         quit()
+                elif event.button==1:
+                    selected="quit"
+                    if quit_blit.collidepoint(event.pos):
+                        print("Collision quit")
+
 
         # Main Menu UI
         screen.fill(BLACK)
-        title = text_format("GoodEye", font, 90, GREEN_YELLOW)
-        if selected == "start":
-            text_start = text_format("Start", font, 60, GREEN_YELLOW)
+        title=text_format("GoodEye", font, 90, GREEN_YELLOW)
+        if selected=="start":
+            text_start=text_format("Start", font, 60, GREEN_YELLOW)
         else:
             text_start = text_format("Start", font, 60, WHITE)
-        if selected == "quit":
-            text_quit = text_format("Quit", font, 60, GREEN_YELLOW)
+        if selected=="quit":
+            text_quit=text_format("Quit", font, 60, GREEN_YELLOW)
         else:
             text_quit = text_format("Quit", font, 60, WHITE)
 
-        title_rect = title.get_rect()
-        start_rect = text_start.get_rect()
-        quit_rect = text_quit.get_rect()
+        title_rect=title.get_rect()
+        start_rect=text_start.get_rect()
+        quit_rect=text_quit.get_rect()
 
         # Main Menu Text
-        screen.blit(title, (screen_width / 2 - (title_rect[2] / 2), 80))
-        screen.blit(text_start, (screen_width / 2 - (start_rect[2] / 2), 300))
-        screen.blit(text_quit, (screen_width / 2 - (quit_rect[2] / 2), 360))
+        screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
+        start_blit = screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))     # Variable now tracks collision
+        quit_blit = screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 360))
         pygame.display.update()
         clock.tick(FPS)
 
-#---------------------------------------------- MAIN MENU ENDS -------------------------------------------------------#
+    # Main game loop
+    game_loop()
 
 
 
-#-------------------------------------------- CountDown Timer ---------------------------------------------------------#
-#------Code Inspired by https://stackoverflow.com/questions/30720665/countdown-timer-in-pygame uploaded by Sloth-------#
-def countdown(time):
+# Draws rectangles dynamically, depending on the data passed
+def drawRect(rects, founds, colors, temp, n):
+    global screen
+    rect = None
 
-    count,text = time, str(time).rjust(10)
-    pygame.time.set_timer(pygame.USEREVENT, 1000)
-    font = pygame.font.SysFont('bold', 50)
-
-    while True:
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                quit()
-
-            if event.type == pygame.USEREVENT:
-                    count -= 1
-                    text = str(count).rjust(10) \
-            if count > 0 else "GAMEOVER."
-
-        else:
-            Rect = pygame.draw.rect(screen, BLACK, (380, 50, 250, 60))
-            screen.blit(font.render(text, True, (255, 255, 255)), (405, 60))
-            pygame.display.flip()
-            pygame.display.update(Rect)
-            clock.tick(FPS)
-            continue
+    # Override display if color found or user is checking the value (1 and 2 clicks)
+    if founds[n] != None or temp[n] != None:
+        rect = pygame.draw.rect(screen, colors[n], rects[n])
+    else:
+        rect = pygame.draw.rect(screen, GRAY, rects[n])
 
 
-
-#-------------------------------------------- CountDown Timer ENDS ----------------------------------------------------#
-
+    return rect
 
 
-#---------------------------------------------- Game Mechanics --------------------------------------------------------#
 
 def game_loop():
     gameExit = False
-    gameOver = False
+    gameOver= False
+    gameStarts = False          # Game starts
+    gamePause = False           # Game pause
+
+    initialTimer = 10     # Initial time for game to start
+
+    lives = 5           # 5 lives total
+
+
+    x = None            # x mouse position
+    y = None            # y mouse position
+
+    clicked = 0         # counts mouse clicks
+
+    level = 1   # levels are signaled by a variable
+
+    # Algebra rectangles where to draw on different levels
+    rects_1 = [(410, 220, 100, 100),(410, 340, 100, 100), (530, 220, 100, 100), (530, 340, 100, 100)]
+    rects_2 = rects_1 + [(410, 100, 100, 100),(530, 100, 100, 100)]
+
+    # Colors
+    colors_1 = [RED, YELLOW, YELLOW, RED]
+    colors_2 = [RED, YELLOW, YELLOW, RED, NAVY_BLUE, NAVY_BLUE]
+
+    # Collision detection variables: PyGame includes collision detection with rectangles
+    collision_1 = [None, None, None, None]
+    collision_2 = [None, None, None, None, None, None]
+
+    # Parallel lists for finding elements
+    found_1 = [None, None, None, None]
+    found_2 = [None, None, None, None, None, None]
+
+    # Parallel lists for temporary clicks - reset if clicks 2
+    temp_1 = [True, True, True, True]
+    temp_2 = [True, True, True, True, True, True]
+
+    wins_1 = [[0,3],[1,2]]              # Lists hold the winning combinations
+    wins_2 = wins_1 + [[4,5]]
+
+
+    currentTicks = 0        # Calls functions 1 per second (accumulates ticks)
+
     while not gameExit:
-        screen.fill(BLACK)
-        pygame.draw.rect(screen, RED, (410, 220, 100, 100))
-        pygame.draw.rect(screen, YELLOW, (410, 340, 100, 100))
-        pygame.draw.rect(screen, YELLOW, (530, 220, 100, 100))
-        pygame.draw.rect(screen, RED, (530, 340, 100, 100))
+        screen.fill(WHITE)
 
-        #mulitple if statements to control time per level
-        #example counter will be the variable to change for each level
-        # if count ==1:
-        #   time = 20;
+        # Display screen depending on the level
+        if level == 1:
+            collision_1[0] = drawRect(rects_1, found_1, colors_1, temp_1, 0)
+            collision_1[1] = drawRect(rects_1, found_1, colors_1, temp_1, 1)
+            collision_1[2] = drawRect(rects_1, found_1, colors_1, temp_1, 2)
+            collision_1[3] = drawRect(rects_1, found_1, colors_1, temp_1, 3)
 
-        time = 10
+        elif level == 2:
+            collision_2[0] = drawRect(rects_2, found_2, colors_2, temp_2, 0)
+            collision_2[1] = drawRect(rects_2, found_2, colors_2, temp_2, 1)
+            collision_2[2] = drawRect(rects_2, found_2, colors_2, temp_2, 2)
+            collision_2[3] = drawRect(rects_2, found_2, colors_2, temp_2, 3)
+            collision_2[4] = drawRect(rects_2, found_2, colors_2, temp_2, 4)
+            collision_2[5] = drawRect(rects_2, found_2, colors_2, temp_2, 5)
 
+
+
+
+        # Timer text
+        timerText = text_format(str(initialTimer), font, 60, GREEN_YELLOW)
+
+        # Live text
+        livesText = text_format('Lives='+str(lives), font, 60, RED)
+
+        # Runs timer 1 per second
+        if gameStarts == False:
+            if initialTimer != 0:
+                screen.blit(timerText, (screen_width / 2 - (screen_height / 2), 300))
+            else:
+                # Game starts
+                gameStarts = True
+                found_1 = [None, None, None, None]
+                found_2 = [None, None, None, None, None, None]
+
+                # Set temporary to None so that there is no more showing of textures
+                temp_1 = [None, None, None, None]
+                temp_2 = [None, None, None, None, None, None]
+
+
+        else:
+            screen.blit(livesText, (100 , 100))   # Display lives
+
+            if initialTimer == 0 and clicked == 2:
+                temp_1 = [None, None, None, None]
+                temp_2 = [None, None, None, None, None, None]
+
+                clicked = 0
+
+        # After draw, check event
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                gameExit = True
+            elif event.type == MOUSEBUTTONDOWN:
+
+                # Only play game is not pause
+                if gameStarts == True:
+                    if level == 1:
+                        # Enumerate means index and value, it is a common practice in python
+                        for i,item in enumerate(collision_1):
+                            # Check if mouse click occurred inside item
+                            if item.collidepoint(event.pos):
+                                # If item was not clicked already
+                                if found_1[i] != True:
+
+                                    clicked += 1  # Always increase counter of clicks if click valid
+
+                                    temp_1[i] = True        # Temporary record which rectangle was clicked
+
+                                    if clicked == 2:
+
+                                        two_clicks = [i for i,v in enumerate(temp_1) if v is not None]
+                                        if two_clicks in wins_1:
+                                            for i in two_clicks:
+                                                found_1[i] = True
+
+                                            # Find out if win
+                                            totalWins = sum([1 for i in found_1 if i is not None])
+                                            # Compare total wins to length of possible total wins
+                                            if totalWins == len(found_1):
+                                                print('Victory')
+                                                level = 2
+
+                                                # Reset all values to start on level 2
+                                                temp_2 = [True, True, True, True, True, True]
+
+                                                lives = 5               # Set lives back to 5
+
+                                                gameStarts = False
+
+                                                initialTimer = 10       # Set timer back to 10
+
+                                                continue            # Jump to next iteration
+
+
+                                            temp_1 = [None, None, None, None]
+                                        else:
+                                            lives -= 1          # Subtract lives
+
+                                            initialTimer = 1    # Set timer to give time to look at different matches (otherwise is instant)
+
+
+
+                    else:
+                        for i,item in enumerate(collision_2):
+                            if item.collidepoint(event.pos):
+                                # If item was not clicked already
+                                if found_2[i] != True:
+
+                                    clicked += 1  # Always increase counter of clicks if click valid
+
+                                    temp_2[i] = True  # Temporary record which rectangle was clicked
+
+                                    if clicked == 2:
+
+                                        two_clicks = [i for i, v in enumerate(temp_2) if v is not None]
+                                        if two_clicks in wins_2:
+                                            for i in two_clicks:
+                                                found_2[i] = True
+                                            # Find out if win
+                                            totalWins = sum([1 for i in found_2 if i is not None])
+                                            # Compare total wins to length of possible total wins
+                                            if totalWins == len(found_2):
+                                                print('Victory')
+                                            temp_2 = [None, None, None, None, None, None]
+                                        else:
+                                            lives -= 1  # Subtract lives
+
+                                            initialTimer = 1  # Set timer to give time to look at different matches (otherwise is instant)
+
+        # Reduce timer only if game has not start
+        if currentTicks == 0:
+            if initialTimer > 0:
+                initialTimer -= 1  # Count down
+        if currentTicks == FPS:
+            currentTicks = 0  # Counts ticks once per second
+        else:
+            currentTicks += 1  # Increase ticks if mark not at FPS (that is if number is not 30)
+
+        pygame.display.update()
         clock.tick(FPS)
-        countdown(time)
 
+
+
+
+    pygame.display.update()
+'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
-
-
-
-
-
-
-#---------------------------------------------- Game Mechanics ENDS ---------------------------------------------------#
-
-# Initialize the Game
+                exit()
+        screen.fill(BLACK)
+        pygame.display.update()
+        clock.tick(FPS)'''
+#Initialize the Game
 main_menu()
-game_loop()
-quit()
+
+#pygame.quit()
+#quit()
